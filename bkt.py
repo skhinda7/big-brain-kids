88ac1ee4b170d1441556e08839218f562965f8ac
Simar <skhinda7@gmail.com>
2023-10-25 16:13:27 -0400

    Completed the sample of AI, ready to implement

--- bkt.py
+++ bkt.py
@@ -3,12 +3,12 @@ taught = 0.1
 slip = 0.3
 guess = 0.2
 
-def correct(known, taught, slip, guess):
+def correct(known, slip, guess):
     num = (known * (1 - slip))
     den = (known * (1 - slip) + (1 - known) * guess)
     return num/den
 
-def incorrect(known, taught, slip, guess):
+def incorrect(known, slip, guess):
     num = (known * slip)
     den = (known * slip + (1 - known) * (1 - guess))
     return num/den
@@ -16,20 +16,18 @@ def incorrect(known, taught, slip, guess):
 def action(known, taught):
     return known + ((1 - known) * taught)
 
-print("Initial Known: ", known)
+def checkSkill(answerList, known, taught):
+    print("Initial Known", known)
+    for i in range(len(answerList)):
+        if(answerList[i] == 0): # Correct Answer
+            known = action(correct(known, slip, guess), taught)
+        if(answerList[i] == 1): # Incorrect Answer
+            known = action(incorrect(known, slip, guess), taught)
+    return known
+    
+testList = [1, 0, 0, 0, 0, 0, 1, 1, 1] # Test Array of Correct/Incorrect Answers
+overall_proficency = checkSkill(testList, known, taught)
+print('Skill Proficency: ', overall_proficency)
 
-altknown = incorrect(known, taught, slip, guess)
-known = action(altknown, taught)
-print("Incorrect Answer: ", known)
-
-altknown = correct(known, taught, slip, guess)
-known = action(altknown, taught)
-print("Correct Answer: ", known)
-
-altknown = correct(known, taught, slip, guess)
-known = action(altknown, taught)
-print("Correct Answer: ", known)
-
-altknown = incorrect(known, taught, slip, guess)
-known = action(altknown, taught)
-print("Final Probability: ", known)
+if(overall_proficency >= 0.95):
+    print("Skill has been mastered: ", overall_proficency)
